2dd6693324e4c9044185d1bd7e90793949d65a72
Ngqabutho K Ndlovu <101482292+kito078@users.noreply.github.com>
2023-09-24 23:02:12 +0200

    s

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="explore" element={<Explore />} />
+        <Route path="offers" element={<Offers />} />
+        <Route path="profile" element={<Profile />} />
+      </Routes>
+      <Navbar />
+    </Router>
   );
 }
 
